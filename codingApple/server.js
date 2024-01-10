bf02b6c9e5fc7ac6174a563f1506f0f7ae94b3e2
codefug <robot9917@naver.com>
2024-01-10 11:27:08 +0900

    add codingapple

--- codingApple/server.js
+++ codingApple/server.js
@@ -1,20 +1,65 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static(__dirname+'/public'))
-app.use(express.static(__dirname+'/public'))
-app.listen(8080, () => {
-    console.log('http://localhost:8080 에서 서버 실행중')
+app.use(express.static(__dirname + '/public'))
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const { MongoClient } = require('mongodb');
+let db;
+const url = 'mongodb+srv://myAtlasDBUser:myAtlasDBUser@myatlasclusteredu.coc8o3e.mongodb.net/?retryWrites=true&w=majority'
+new MongoClient(url).connect().then((client) => {
+    console.log('DB연결 성공');
+    db = client.db('forum');
+    app.listen(8080, () => {
+        console.log('http://localhost:8080 에서 서버 실행중');
+    })
+}).catch((err) => {
+    console.log(err);
 })
 app.get('/', (요청, 응답) => {
-    응답.sendFile(__dirname+'/index.html')
+    응답.sendFile(__dirname + '/index.html');
 })
 app.get('/URL', (요청, 응답) => {
-    응답.send('오늘 비옴')
+    응답.send('오늘 비옴');
 })
 app.get('/shop', (요청, 응답) => {
-    응답.send('쇼핑페이지임')
+    응답.send('쇼핑페이지임');
 })
-app.get('/about',(요청, 응답)=>{
-    응답.sendFile(__dirname+'/index2.html')
+app.get('/about', (요청, 응답) => {
+    응답.sendFile(__dirname + '/index2.html')
+})
+app.get('/news', async (요청, 응답) => {
+    응답.send('오늘 비옴')
+})
+app.get('/list', async (요청, 응답) => {
+    let result = await db.collection('post').find().toArray()
+    console.log(result)
+    응답.send('DB에 있던 게시물')
+});
+app.get('/write', (요청, 응답) => {
+    const 글목록 = [
+        { title: '글1', content: '내용1' },
+        { title: '글2', content: '내용2' },
+        { title: '글3', content: '내용3' },
+    ];
+
+    응답.render('write.ejs', { 글목록 })
+})
+app.post('/add', async (요청, 응답) => {
+    console.log(요청.body)
+
+    try{
+        if (요청.body.title==''){
+           응답.send('제목입력안했는데?')
+        }else{
+            await db.collection('post').insertOne({title: 요청.body.title,content
+                :요청.body.conent})
+        }
+    }catch(e){
+        console.log(e)
+        응답.status(500).send('서버에러남')
+    }
+    응답.redirect('/list')
 })
