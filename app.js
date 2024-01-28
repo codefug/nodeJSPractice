1832a4a9ae56ae98d80f2d3e61b220b674f4236f
codefug <86544979+codefug@users.noreply.github.com>
2024-01-28 17:48:49 +0900

    success accessment of db

--- app.js
+++ app.js
@@ -1,55 +1,43 @@
-const dotenv = require("dotenv");
-dotenv.config();
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-// const port = 3000;
-// const express = require("express");
-// const app = express();
-// const morgan = require("morgan");
-// const cookieParser = require("cookie-parser");
-// const session = require("express-session");
+// const { MongoClient } = require("mongodb");
 
-// app.use(morgan("dev"));
-// app.use(cookieParser(process.env.COOKIE_SECRET));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(
-//   session({
-//     resave: false,
-//     saveUninitialized: false,
-//     secret: process.env.COOKIE_SECRET,
-//     cookie: {
-//       httpOnly: true,
-//     },
-//   })
-// );
+// // Replace the uri string with your connection string.
+// const uri =
+//   `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@cluster0.tfhjsuj.mongodb.net/?retryWrites=true&w=majority`;
 
-// app.get("/", (req, res) => {
-//   req.signedCookies;
-// });
+// const client = new MongoClient(uri);
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
+// async function run() {
+//   try {
+//     const database = client.db("criteria");
+//     const movies = database.collection("exam");
+//     const name_list = ["TOEIC","TOEIC('IBT')","NEW TEPS","IELTS","TOEIC SPEAKING","TOEIC WRITING","OPIC"];
+//     const condition_listj = [["600"],["68"],["227"],["5.5"],["110"],["120"],["IL","IM","IH","AL"]];
+//     const condition_listy = [["500"],["53"],["195"],["4.5"],["100"],["105"],["NH", "IL","IM","IH","AL"]];
+//     const type_list = ["주간","야간"];
+//     let add;
+//     for (const type of type_list) {
+//         if (type=="주간"){
+//             for (const name of name_list){
+//                 add = await movies.insertOne({"name":name,"type":type,"condition":condition_listj[name_list.indexOf(name)]});
+//             }
+//         }
+//         else{
+//             for (const name of name_list) {
+//               add = await movies.insertOne({
+//                 name: name,
+//                 type: type,
+//                 condition: condition_listy[name_list.indexOf(name)],
+//               });
+//             }
+//         }
+//     }
 
-const { MongoClient } = require("mongodb");
-
-// Replace the uri string with your connection string.
-const uri =
-  `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@cluster0.tfhjsuj.mongodb.net/?retryWrites=true&w=majority`;
-
-const client = new MongoClient(uri);
-
-async function run() {
-  try {
-    const database = client.db("criteria");
-    const movies = database.collection("exam");
-
-    const query = { name: "TOEIC",type: "야간", condition: [500]};
-    const movie = await movies.insertOne(query);
-
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
