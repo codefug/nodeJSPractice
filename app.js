5e30196e4a1552ab16f563f8615dd48007703e71
codefug <86544979+codefug@users.noreply.github.com>
2024-01-28 18:22:02 +0900

    complete making user data

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-// const dotenv = require("dotenv");
-// dotenv.config();
+const dotenv = require("dotenv");
+dotenv.config();
 
-// const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
 
 // // Replace the uri string with your connection string.
-// const uri =
-//   `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@cluster0.tfhjsuj.mongodb.net/?retryWrites=true&w=majority`;
+const uri =
+  `mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@cluster0.tfhjsuj.mongodb.net/?retryWrites=true&w=majority`;
 
-// const client = new MongoClient(uri);
-
+const client1 = new MongoClient(uri);
+const client2 = new MongoClient(uri);
+// 시험 추가에 쓴 코드
 // async function run() {
 //   try {
 //     const database = client.db("criteria");
@@ -38,6 +39,30 @@
 //   } finally {
 //     // Ensures that the client will close when you finish/error
 //     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+// 몽고디비랑 연결해서 user데이터 뽑아봤음.
+// async function run() {
+//   try {
+//     const database = client1.db("userData");
+//     const movies = database.collection("users");
+//     const database2 = client2.db("timeTable");
+//     const movies2 = database2.collection("2019_1");
+//     const l = [[],[],[],[]];   
+
+//     const subjects = movies2.find();
+//     for await (const subject of subjects){
+//         l[0].push(subject);
+//         console.log(subject)
+//     }
+//     const movie = await movies.insertOne({"name":"firstuser","article":l});
+
+//     console.log(movie);
+//   } finally {
+//     await client1.close();
+//     await client2.close();
 //   }
 // }
 // run().catch(console.dir);
