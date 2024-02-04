5e678e464a9df6ffb1679e45b10d93146de07ea3
codefug <robot9917@naver.com>
2024-02-04 23:08:20 +0900

    add params system

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const dotenv = require("dotenv");
-dotenv.config();
+// const dotenv = require("dotenv");
+// dotenv.config();
 
 // const { MongoClient } = require("mongodb");
 
@@ -66,57 +66,57 @@ dotenv.config();
 // }
 // run().catch(console.dir);
 
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
-const {
-  OAUTH_USER,
-  OAUTH_CLIENT_ID,
-  OAUTH_CLIENT_SECRET,
-  OAUTH_REFRESH_TOKEN,
-} = process.env;
-console.log(OAUTH_CLIENT_ID);
-async function main(receiverEmail) {
+// const {
+//   OAUTH_USER,
+//   OAUTH_CLIENT_ID,
+//   OAUTH_CLIENT_SECRET,
+//   OAUTH_REFRESH_TOKEN,
+// } = process.env;
+// console.log(OAUTH_CLIENT_ID);
+// async function main(receiverEmail) {
 
-  const transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: 'smtp.google.com',
-    port: 587,
-    secure: true,
-    auth: {
-      type: 'OAuth2',
-      user: `${OAUTH_USER}`,
-      clientId: `${OAUTH_CLIENT_ID}`,
-      clientSecret: `${OAUTH_CLIENT_SECRET}`,
-      refreshToken: `${OAUTH_REFRESH_TOKEN}`,
-    },
-  });
+//   const transporter = nodemailer.createTransport({
+//     service: 'gmail',
+//     host: 'smtp.google.com',
+//     port: 587,
+//     secure: true,
+//     auth: {
+//       type: 'OAuth2',
+//       user: `${OAUTH_USER}`,
+//       clientId: `${OAUTH_CLIENT_ID}`,
+//       clientSecret: `${OAUTH_CLIENT_SECRET}`,
+//       refreshToken: `${OAUTH_REFRESH_TOKEN}`,
+//     },
+//   });
   
-  const message = {
-    from: OAUTH_USER,
-    to: receiverEmail,
-    subject: '제발..',
-    html: `
-      <h1>
-        Nodemailer X Gmail OAuth 2.0 테스트 메일
-      </h1>
-      <hr />
-      <br />
-      <p>한시름 놓았어..<p/>
-      <p>그만 하자 이제 제발. .. </p>
-      <br />
-      <hr />
-      <p>이 메일은 Gmail API를 써보고 싶은 정신나간 개발자에 의해서 발송되었습니다.</p>
-      <p>이 메일을 요청한 적이 없으시다면 무시하시기 바랍니다.</p>
-    `,
-  };
+//   const message = {
+//     from: OAUTH_USER,
+//     to: receiverEmail,
+//     subject: '제발..',
+//     html: `
+//       <h1>
+//         Nodemailer X Gmail OAuth 2.0 테스트 메일
+//       </h1>
+//       <hr />
+//       <br />
+//       <p>한시름 놓았어..<p/>
+//       <p>그만 하자 이제 제발. .. </p>
+//       <br />
+//       <hr />
+//       <p>이 메일은 Gmail API를 써보고 싶은 정신나간 개발자에 의해서 발송되었습니다.</p>
+//       <p>이 메일을 요청한 적이 없으시다면 무시하시기 바랍니다.</p>
+//     `,
+//   };
   
-  try {
-    await transporter.sendMail(message);
-    console.log('메일을 성공적으로 발송했습니다.');
-  } catch (e) {
-    console.log(e);
-  }
+//   try {
+//     await transporter.sendMail(message);
+//     console.log('메일을 성공적으로 발송했습니다.');
+//   } catch (e) {
+//     console.log(e);
+//   }
 
-}
+// }
 
-main("robot9917@naver.com");
+// main("robot9917@naver.com");
---
+++ nodebird/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('/:id',(req, res, next) => {
+  console.log(req.params.id);
+  console.log('노드 버드 프로젝트를 시작합니다.')
+  next();
+})
+
+app.get('/', (req, res) => {/*
+root URL(route)와 연결
+req, res는 Node 환경에서 제공해주는 것과 같다. 따라서 req.pipe(), 
+req.on('data', callback)과 같은 것을 호출할 수 있다.
+*/
+  res.send('Hello World!') // Hello World!라는 데이터 전송
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})// 3000 포트와 연결
